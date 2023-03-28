526f15047850cbae5972dcd7b00435f38f9264ed
AngelikaSzor <128865633+AngelikaSzor@users.noreply.github.com>
2023-03-28 10:09:14 +0200

    Update lista.cpp

--- lista.cpp
+++ lista.cpp
@@ -5,9 +5,11 @@ Lista::Lista()
     this->head = nullptr;
     this->tail = nullptr;
     this->rozmiar = 0;
+    //std::cout<<"[d] utworzono liste head: " << this->head<<'\n';
 }
 Lista::~Lista()
 {
+    //std::cout << "[d] usuwam liste head: " << this->head;
     while(this->head)
     {
        Element_listy* temp = this->head;
@@ -15,6 +17,8 @@ Lista::~Lista()
        delete temp;
     }
     this->tail=nullptr;
+
+    //std::cout<<"[d] usunieto liste\n";
 }
 
 void Lista::dodaj_na_koncu(std::string &napis)
@@ -25,45 +29,20 @@ void Lista::dodaj_na_koncu(std::string &napis)
     if(this->head == nullptr)
     {
         this->head = temp;
-       // std::cout<<"head: "<<this->head<<'\n';
+        //std::cout<<"head: "<<this->head<<'\n';
         this->tail = temp;
     }
     else
     {
         this->tail->next = temp;
-       //std::cout<<"tail: "<<this->tail<< ' ' << "tail->next: " << tail->next << '\n';
+        //std::cout<<"tail: "<<this->tail<< ' ' << "tail->next: " << tail->next << '\n';
         this->tail = this->tail->next;
     }
     this->rozmiar ++;
-   //std::cout << this->rozmiar << '\n';
-}
-void Lista::drukuj_liste()
-{
-    Element_listy* it = this->head;
-    int iteracja=1;
-    while(it)
-    {
-        std::cout<<iteracja<<". "<<it->dane<<'\n';
-        it = it->next;
-        iteracja++;
-    }
+    //std::cout << "rozmiar: " << this->rozmiar << '\n';
+   //std::cout<<"[d] dodano element na koniec\n";
 }
 
-void Lista::getDane(int indeks, std::string &dane)
-{
-    Element_listy *temp;
-    temp = this->head;
-    for(int i = 0; i < indeks; i++)
-    {
-        temp = temp->next;
-    }
-    dane = temp->dane;
-}
-
-int Lista::get_rozmiar()
-{
-    return this->rozmiar;
-}
 std::string Lista::wyjmij_z_listy(int indeks)
 {
     std::string wyjety;
@@ -90,5 +69,67 @@ std::string Lista::wyjmij_z_listy(int indeks)
 
     }
     this->rozmiar--;
+    //std::cout<<"[d] wyjeto element z listy\n";
     return wyjety;
 }
+
+void Lista::drukuj_liste()
+{
+    Element_listy* it = this->head;
+    int iteracja=0;
+    while(it)
+    {
+        std::cout<<iteracja<<". "<<it->dane<<'\n';
+        it = it->next;
+        iteracja++;
+    }
+
+    //std::cout <<"[d] wydrukowano liste\n";
+}
+int Lista::sprawdzenie_int()
+{
+    int n;
+    std::cin >> n;
+    while (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore();
+        std::cout<<"Niepoprawny numer. Prosze wpisac ponownie: \n";
+        std::cin>>n;
+    }
+    std::cin.sync();
+    std::cout<<"[d] pobrano bezpiecznie inta\n";
+    return n;
+}
+void Lista::getDane( std::string &dane)
+{
+
+    Element_listy *temp;
+    temp = this->head;
+    int indeks = 0;
+    indeks = sprawdzenie_int();
+    while(indeks<-1 || indeks>3)
+    {
+        if(indeks>-1 && indeks<4)
+        {
+            for(int i = 0; i < indeks+1; i++)
+            {
+                temp = temp->next;
+            }
+            dane = temp->dane;
+        }
+        else
+        {
+            std::cout<<"\nPodaj poprawny numer wybranej bazy hasel\n";
+            indeks = sprawdzenie_int();
+        }
+
+    }
+}
+
+int Lista::get_rozmiar()
+{
+    return this->rozmiar;
+}
+
+
